c4292886cbafb03a89079d75baf02610d7c643c6
Amit <amit@divine-it.net>
2021-07-16 04:15:21 +0600

    About us Page design

--- builderapp/urls.py
+++ builderapp/urls.py
@@ -4,4 +4,5 @@ from. import views
 
 urlpatterns = [
     path('',views.home,name = 'home'),
+    path('aboutus',views.aboutus, name='aboutus')
 ]
--- builderapp/views.py
+++ builderapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
